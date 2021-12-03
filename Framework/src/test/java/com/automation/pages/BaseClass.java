77e5e584ab160fce8cf12299147ae6acc2280dc6
ckakarla <ckakarla@192.168.0.101>
2021-12-03 14:53:59 +0530

    Updated pom for Maven build parameterization from cli

--- Framework/src/test/java/com/automation/pages/BaseClass.java
+++ Framework/src/test/java/com/automation/pages/BaseClass.java
@@ -8,6 +8,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 import com.automation.utility.BrowserFactory;
 import com.automation.utility.ConfigDataProvider;
@@ -27,35 +28,38 @@ public class BaseClass {
 	public ConfigDataProvider config;
 	public ExtentReports report;
 	public ExtentTest logger;
+	String htmlReportPath = System.getProperty("user.dir") + "/ExtentReports/FreeCRMReport" + Helper.getTimeMillSec()
+			+ ".html";
 
 	@BeforeSuite
 	public void setUpSuite() {
+
 		Reporter.log("Setting up reports for test !!", true);
 
 		excel = new ExcelDataProvider();
 		config = new ConfigDataProvider();
 
-		ExtentSparkReporter spark = new ExtentSparkReporter(
-				new File("./ExtentReports/FreeCRMReport" + Helper.getTimeMillSec() + ".html"));
+		ExtentSparkReporter spark = new ExtentSparkReporter(new File(htmlReportPath));
 		report = new ExtentReports();
 		report.attachReporter(spark);
 		Reporter.log("Settings done for report - Test can begin !!", true);
 	}
 
+	@Parameters({ "browser", "urlToTest" })
 	@BeforeClass
-	public void setUp() {
+	public void setUp(String browser, String url) {
 
-		Reporter.log("Trying to start browser & Getting app ready!!",true);
-		driver = BrowserFactory.startApplication(driver, config.getBrowser(), config.getStagingURL());
-		Reporter.log("Browser launched and App is up and running!!",true);
+		Reporter.log("Trying to start browser & Getting app ready!!", true);
+//		driver = BrowserFactory.startApplication(driver, config.getBrowser(), config.getStagingURL());
+		driver = BrowserFactory.startApplication(driver, browser, url);
+		Reporter.log("Browser launched and App is up and running!!", true);
 	}
-
 
 	@AfterMethod
 	public void tearDownMethod(ITestResult result) {
-		
-		Reporter.log("Test is about to end!!",true);
-		
+
+		Reporter.log("Test is about to end!!", true);
+
 		if (result.getStatus() == ITestResult.FAILURE) {
 
 			logger.fail("Test failed !!",
@@ -67,16 +71,20 @@ public class BaseClass {
 		}
 
 		report.flush();
-		
-		Reporter.log("Test completed >>> Reports are generated!!",true);
+
+		Reporter.log("Test completed >>> Reports are generated!!", true);
 	}
-	
-	
+
 	@AfterClass
 	public void tearDown() {
 		BrowserFactory.quitBrowser(driver);
-		
 		Reporter.log("\n****************  Browser closed   ********************\n",true);
+		
+		Reporter.setEscapeHtml(true);
+
+	    Reporter.log("<a href='" + htmlReportPath + "'><font color='red'>View Report</font></a>", true);
+//		Reporter.log("===================== Extent Reports can view Link:---> "+htmlReportPath,true);
+		
 	}
 
 }
